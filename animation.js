c5cb487ff29ae9615513e9f33231ca4bba39c516
Jie Wei <xjiewei95@gmail.com>
2021-02-01 15:44:35 +0800

    animation js update

---
+++ animation.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    let clickIntro = 0;
+    $("#intro").click(function(){
+        console.log($(this));
+        $("#introContent").slideToggle("slow");
+        if (clickIntro == 0) {
+            $(this).css("width", "+=20%");
+            clickIntro = 1;
+        } else {
+            $("#intro").css("width", "-=20%");
+            clickIntro = 0;
+        }
+
+    });
+
+    $("#workHead").click(function(){
+        $("#workContent").slideToggle("slow");
+    });
+
+    $("#educationHead").click(function(){
+        $("#educationContent").slideToggle("slow");
+    });
+
+    $("#projectHead").click(function(){
+        $("#projectContent").slideToggle("slow");
+    });
+
+    $("#ccaHead").click(function(){
+        $("#ccaContent").slideToggle("slow");
+    });
+});
